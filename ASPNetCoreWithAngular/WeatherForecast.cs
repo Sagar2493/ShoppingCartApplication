2a6bd050abc2eecfdb3efaf6d88af75304d9b2ab
Sagar Chowdhury <sagar.bzn@gmail.com>
2021-12-10 01:21:51 +0530

    Added EntityFramework packages

--- ASPNetCoreWithAngular/WeatherForecast.cs
+++ ASPNetCoreWithAngular/WeatherForecast.cs
@@ -11,7 +11,5 @@ namespace ASPNetCoreWithAngular
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
-
-        public string Value { get; set; }
     }
 }
